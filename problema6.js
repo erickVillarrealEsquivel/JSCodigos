fc8caa8fca0b7c21cf31bc5b8c944e0944b67d36
Erick Villarreal <a310905@uach.mx>
2019-09-17 17:43:28 -0500

    6.- Hacer un programa que imprima todos los números naturales que hay desde el uno hasta un número que introducimos por teclado.

---
+++ problema6.js
@@ -0,0 +1,8 @@
+console.log("Escribe un numero");
+var numero=process.openStdin();
+numero.on('data', function(chunk) {
+  for(var i=0;i<=chunk;i++){
+      console.log(i);
+  }
+  console.log("escribe otro numero o control+C para salir")
+ });
