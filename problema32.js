87e4bd6473d0ae0e746fcd6570b5221c45b1cd7c
Erick Villarreal <a310905@uach.mx>
2019-09-18 16:39:20 -0500

    32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta.

---
+++ problema32.js
@@ -0,0 +1,4 @@
+var arreglo=["p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12","p13","p14","p15","p16","p17","p18","p19","p20"];
+for (var i = 0; i < arreglo.length; i++) {
+  console.log(arreglo[i]);
+}
