88809786ec5129d06d750010f509b10d32d37360
Erick Villarreal <a310905@uach.mx>
2019-09-18 00:21:26 -0500

    23.- Hacer un programa que imprima los números del 0 al 100, controlando las filas y las columnas.

---
+++ problema24.js
@@ -0,0 +1,17 @@
+console.log("Escribe un numero");
+var numero=process.openStdin();
+var ban=true;
+numero.on('data', function(chunk) {
+  for(var i=2;i<chunk;i++){
+      if(chunk%i==0){
+        ban=false;
+      }
+  }
+  if (ban) {
+  console.log("es primo");
+  }else {
+    console.log("no es primo");
+  }
+  ban=true;
+  console.log("Escribe otro numero o CONTROL+C para salir");
+});
