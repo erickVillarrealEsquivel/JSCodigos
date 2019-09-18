1e2168eeb1fe9b9948b78a0801938fea24497369
Erick Villarreal <a310905@uach.mx>
2019-09-18 18:32:36 -0500

    44.- Con el archivo DATOS.DAT listar todos los registros cuyo estado sea uno determinado que introduciremos por teclado.

---
+++ problema44.js
@@ -0,0 +1,38 @@
+function Dato(id, nombre,apellido,direccion,estado) {
+    this.id = id;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.direccion = direccion;
+    this.estado = estado;
+    this.setId = function(id) {
+       this.id=id;
+   }
+   this.setNombre = function(nombre) {
+      this.nombre=nombre;
+  }
+  this.setApellido = function(apellido) {
+     this.apellido=apellido;
+ }
+ this.setApellido = function(apellido) {
+    this.apellido=apellido;
+}
+this.setDireccion = function(direccion) {
+   this.direccion=direccion;
+}
+this.setEstado = function(estado) {
+   this.estado=estado;
+}
+   this.getId = function() {
+      return this.id;
+  }
+  this.getEstado = function() {
+     return this.estado;
+ }
+}
+dato1=new Dato(1,1,1,1,1);
+dato1.setId(2);
+console.log(dato1.getId());
+
+if(dato1.getEstado="1"){
+  console.log(dato1.getEstado);
+}
