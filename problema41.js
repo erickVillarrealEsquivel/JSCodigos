6b2e8412df7189432d8247c7d9c4717a63859c0d
Erick Villarreal <a310905@uach.mx>
2019-09-18 18:18:38 -0500

    41. Hacer un programa que nos permita dar altas en el archivo DATOS.DAT, cuyos campos son: ID, NOMBRE, APELLIDOS, DIRECCIÓN y ESTADO.

---
+++ problema41.js
@@ -0,0 +1,8 @@
+function Dato(id, nombre,apellido,direccion,estado) {
+    this.id = id;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.direccion = direccion;
+    this.estado = estado;
+}
+dato1=new Dato(1,1,1,1,1);
